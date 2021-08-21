4a54f0c69b655acba43aab04412bcae8d6dc70f8
Noxilla <89015778+Noxilla@users.noreply.github.com>
2021-08-21 21:56:42 +0100

    Add files via upload

---
+++ Html/nname.js
@@ -0,0 +1,5 @@
+my = function(){
+	var first = document.getElementbyId ("firstname").value;
+	var second = document.getElementbyId ("lastname").value;
+document.getElementById("here").innerHTML=first""+""+second
+}
